294cc7866d42beb18ad995c111546a153d6ac207
sebastian krnjus <sebastiankrnjus25@gmail.com>
2022-09-12 12:10:35 +0200

    Add project files.

---
+++ zadatakbr5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatakbr5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("upisi domenu");
+            string upis = Console.ReadLine();
+
+            if(upis.Substring(0, 3) == "www")
+            {
+                if(upis.Substring(upis.Length-3) == ".hr")
+                {
+                    Console.WriteLine("tocna adresa");
+                }
+                else
+                {
+                    Console.WriteLine("netocna adresa");
+                }
+            }
+            else
+            {
+            Console.WriteLine("netocna adresa");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
